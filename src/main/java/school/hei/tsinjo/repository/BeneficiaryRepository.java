8d1c0cff2b1e6d557967b26c8522493356c4f171
Harentsoa <hei.harentsoa@gmail.com>
2025-08-05 17:04:55 +0300

    feat: tsinjo

--- src/main/java/school/hei/tsinjo/repository/BeneficiaryRepository.java
+++ src/main/java/school/hei/tsinjo/repository/BeneficiaryRepository.java
@@ -1,6 +1,7 @@
 package school.hei.tsinjo.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import school.hei.tsinjo.entity.Beneficiary;
 
